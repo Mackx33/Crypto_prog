321c0be57a336b4d7b7429f433419a79539f715a
Mackx33 <maxime.martinez@ynov.com>
2018-03-26 11:43:00 +0200

    Latest version

--- crypto.py
+++ crypto.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3.6
 #-*- coding: utf-8 -*-
 
-#eliott.bureau@ynov.com
-
 import os
 import cryptocompare
 
@@ -18,21 +16,3 @@ while True:
         for [key,value] in cryptocompare.get_coin_list(format=False).items():
             if selection.lower() == value.get("CoinName").lower():
                 print(value.get("CoinName")+" : "+str(cryptocompare.get_price(key)))
-
-
-#while 1:
-#    selection = int(input("Veuillez choisir une cryptomoney : "))
-#    if selection == 'Id':
-#        crypto_result = cryptocompare.get_price(cryptocompare.get_coin_Id)
-#        print(crypto_result)
-#    break
-
-#while 1:
-#    select_one = int(input("Choisissez votre cryptomoney \n 1. Bitcoins \n 2. Ethereum \n Votre choix : "))
-#    if select_one == 1:
-#        crypto_one = cryptocompare.get_price('BTC')
-#        print (crypto_one)
-#    elif select_one == 2:
-#        crypto_two = cryptocompare.get_price('ETH')
-#        print (crypto_two)
-#    break
